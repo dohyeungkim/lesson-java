584c45cc5893cceedb0f90abc069fe4e61f660c9
gimdohyeong <kdh990624@naver.com>
2024-06-23 16:31:20 -0700

    예시

---
+++ app/src/main/java/lesson/java/variable/Main2.java
@@ -0,0 +1,9 @@
+package lesson.java.variable;
+//컴파일에 대해서 설명해보세요. :
+public class Main2 {
+    public static void main(String[] args) {
+        int k = 4;
+        long g = 500_000_000_000L;
+        System.out.println(k+g);
+    }
+}
