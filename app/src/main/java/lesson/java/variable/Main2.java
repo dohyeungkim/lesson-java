cc92bf2e319e226a699af7368bdb0d3c7df0ea35
gimdohyeong <kdh990624@naver.com>
2024-06-25 20:10:04 -0700

    과제2

--- app/src/main/java/lesson/java/variable/Main2.java
+++ app/src/main/java/lesson/java/variable/Main2.java
@@ -1,5 +1,5 @@
 package lesson.java.variable;
-//컴파일에 대해서 설명해보세요. : ㅇㅇㅇ
+//컴파일에 대해서 설명해보세요. :컴파일은 자바 소스 코드를 기계가 이해할 수 있는 형태로 변환하는 과정입니다. 이 과정을 통해 자바 프로그램은 실행 가능한 형태로 변환됩니다. 프로그래머는 java확장자를 통해 소스 코드을 만들고 컴파일하면 바이트코드를 생성합니다. 생성한 바이트 코드를 JVM 가상 머신을 통해 실행합니다.
 public class Main2 {
     public static void main(String[] args) {
         int k = 4;
