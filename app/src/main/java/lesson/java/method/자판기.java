5b4c12bf1b97454844dc6e98894e50b33d8d747d
gimdohyeong <kdh990624@naver.com>
2024-07-02 21:08:37 -0700

    자판기3

--- app/src/main/java/lesson/java/method/자판기.java
+++ app/src/main/java/lesson/java/method/자판기.java
@@ -1,38 +1,49 @@
 package lesson.java.method;
-
-public class 자판기 {
-       String cocacola;
-       String cider;
-       String water;
-
+     public class 자판기 {
+         String[] beverageNames = {"스프라이트", "콜라", "밀키스"};
+         int[] beverageCost = {1100, 900, 1400};
+         int[] beverageStock = {4, 3, 2};
 
 
-    int money(){
-        return 3000;
-    }
-    String choice1(){
-        return cocacola;
-    }
-    String choice2(){
-        return cider;
-    }
-    String choice3(){
-        return water;
-    }
-    String nostorage(){
-        if(true){
-          return "재고가 없습니다";
-        }
-        return "음료를 사기에 투입된 돈이 충분하지 않다.";
-    }
+         String printbeverages1() {
+              return beverageNames[0];
+         }
+         String printbeverages2(){
+              return beverageNames[1];
+         }
+         String printbeverages3(){
+             return beverageNames[2];
+         }
+         int Cost1(){
+             return beverageCost[0];
+         }
+         int Cost2(){
+             return beverageCost[1];
+         }
+         int Cost3(){
+             return beverageCost[2];
+         }
+         void stock1() {
+             for (int i=0;i<beverageStock[0];i++) {
+                 beverageStock[0]--;
+             }
+         }
+         void stock2() {
+             for(int i=0; i<beverageStock[1];i++){
+                 beverageStock[1]--;
+             }
+         }
+         void stock3() {
+             for(int i=0;i<beverageStock[2];i++){
+                 beverageStock[2]--;
+             }
 
-    public static void main(String[] args) {
-        자판기 a = new 자판기();
-        a.choice1();
-        a.choice2();
-        a.choice3();
+         }
 
-    }
+         public static void main(String[] args){
+
+
+         }
 
 
 }
