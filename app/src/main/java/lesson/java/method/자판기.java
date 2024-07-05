4e8b1366e4e64c91e0a83d6bdd0116ca5c8c7d8a
gimdohyeong <kdh990624@naver.com>
2024-07-04 17:28:36 -0700

    자판기 수정본

--- app/src/main/java/lesson/java/method/자판기.java
+++ app/src/main/java/lesson/java/method/자판기.java
@@ -1,49 +1,60 @@
 package lesson.java.method;
      public class 자판기 {
-         String[] beverageNames = {"스프라이트", "콜라", "밀키스"};
-         int[] beverageCost = {1100, 900, 1400};
-         int[] beverageStock = {4, 3, 2};
 
-
-         String printbeverages1() {
-              return beverageNames[0];
-         }
-         String printbeverages2(){
-              return beverageNames[1];
-         }
-         String printbeverages3(){
-             return beverageNames[2];
-         }
-         int Cost1(){
-             return beverageCost[0];
-         }
-         int Cost2(){
-             return beverageCost[1];
-         }
-         int Cost3(){
-             return beverageCost[2];
-         }
-         void stock1() {
-             for (int i=0;i<beverageStock[0];i++) {
-                 beverageStock[0]--;
-             }
-         }
-         void stock2() {
-             for(int i=0; i<beverageStock[1];i++){
-                 beverageStock[1]--;
-             }
-         }
-         void stock3() {
-             for(int i=0;i<beverageStock[2];i++){
-                 beverageStock[2]--;
+         void printbeverages(String[] b_N) {
+             for(int i=0;i<b_N.length;i++){
+                 System.out.println(b_N[i]);
              }
 
          }
+         int takeamoney(int a){
+             int[] money = {1500,2000,800};
+             for(int i=0;i<money.length;i++){
+                 a = money[i];
+             }
+             return a;
+         }
+
+         String choicebeverage(String b){
+             String[]beverage = {"스프라이트","콜라","밀키스"};
+             for(int i=0;i<beverage.length;i++) {
+                  b = beverage[i];
+             }
+             return b;
+         }
+         void stockconfirm(int b_Stock,int b_Cost){
+             if(b_Stock == 0){
+                 System.out.println("음료가 없습니다.");
+             }else{
+                 System.out.println("음료를 사기에 투입된 돈이 충분하지 않다.");
+                 if(true){
+                     System.out.println("금액이 부족합니다.");
+                 }else{
+
+                     b_Stock--;
+                     System.out.println("음료를 구매했습니다.");
+                 }
+             }
+
+         }
+
+
+
 
          public static void main(String[] args){
+             String[] beverageNames = {"스프라이트", "콜라", "밀키스"};
+             int[] beverageCost = {1100, 900, 1400};
+             int[] beverageStock = {4, 3, 2};
+
+             자판기 printbeverage = new 자판기();
+             자판기 stock = new 자판기();
+             printbeverage.printbeverages(beverageNames);
+
+
 
 
          }
+
 
 
 }
